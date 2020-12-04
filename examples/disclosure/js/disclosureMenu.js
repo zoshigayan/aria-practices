6086d3edde432890d0ee5ef8a9feff2eb4cd88c7
Valerie Young <valerie@bocoup.com>
2020-12-04 13:32:42 -0800

    Do not expect 'include optional support' in example

--- examples/disclosure/js/disclosureMenu.js
+++ examples/disclosure/js/disclosureMenu.js
@@ -175,12 +175,14 @@ window.addEventListener(
 
     // listen to arrow key checkbox
     var arrowKeySwitch = document.getElementById('arrow-behavior-switch');
-    arrowKeySwitch.addEventListener('change', function (event) {
-      var checked = arrowKeySwitch.checked;
-      for (var i = 0; i < disclosureMenus.length; i++) {
-        disclosureMenus[i].updateKeyControls(checked);
-      }
-    });
+    if (arrowKeySwitch) {
+      arrowKeySwitch.addEventListener('change', function (event) {
+        var checked = arrowKeySwitch.checked;
+        for (var i = 0; i < disclosureMenus.length; i++) {
+          disclosureMenus[i].updateKeyControls(checked);
+        }
+      });
+    }
 
     // fake link behavior
     var links = document.querySelectorAll('[href="#mythical-page-content"]');
